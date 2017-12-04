fcbb7c50ff3e9daf917b499b620aedce3f9aa0a9
coomeva <34242282+coomeva@users.noreply.github.com>
2017-12-04 16:42:46 -0500

    Update index.js

--- index.js
+++ index.js
@@ -13,9 +13,12 @@ restService.use(bodyParser.json());
 
 restService.post('/echo', function(req, res) {
     var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Seems like some problem. Speak again."
+    
+    var sum = speech + speech;
+    
     return res.json({
-        speech: speech,
-        displayText: speech,
+        speech: sum,
+        displayText: sum,
         source: 'webhook-echo-sample'
     });
 });
