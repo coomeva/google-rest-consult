470bb6ef43d2ea63bb23676a987039fc6240412e
coomeva <34242282+coomeva@users.noreply.github.com>
2017-12-04 16:39:42 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-
 const restService = express();
 
 restService.use(bodyParser.urlencoded({
@@ -13,16 +12,12 @@ restService.use(bodyParser.urlencoded({
 restService.use(bodyParser.json());
 
 restService.post('/echo', function(req, res) {
-    
-       var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Seems like some problem. Speak again."
-     
-    
+    var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Seems like some problem. Speak again."
     return res.json({
         speech: speech,
         displayText: speech,
         source: 'webhook-echo-sample'
-        });
-    
+    });
 });
 
 restService.post('/music', function(req, res) {
@@ -106,15 +101,6 @@ restService.post('/slack-test', function(req, res) {
     });
 });
 
-unction callConsultAssociate(number){
-    return new Promise((resolve, reject) => {
-        
-        var suma = number + number ;
-        
-        resolve(suma);
-        
-    });
-}
 
 
 
