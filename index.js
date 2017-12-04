c768e58b8fb78be95c1a67b575fd43d33c552bec
coomeva <34242282+coomeva@users.noreply.github.com>
2017-12-04 17:53:49 -0500

    Update index.js

--- index.js
+++ index.js
@@ -14,16 +14,17 @@ restService.use(bodyParser.json());
 
 restService.post('/echo', function(req, res) {
     var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Seems like some problem. Speak again."
-    var speech2 = 'consume service';
-     callConsultAssociate(speech).then((resultado) => {
-      speech2 = ' ' + resultado;
-    });
     
-   return res.json({
+     callConsultAssociate(speech).then((resultado) => {
+      var speech2 = ' ' + resultado;
+         return res.json({
         speech: speech2,
         displayText: speech2,
         source: 'webhook-echo-sample'
     });
+    });
+    
+   
 });
 
 restService.post('/music', function(req, res) {
