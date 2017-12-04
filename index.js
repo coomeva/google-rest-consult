a1316dacd8f4550997d030ab2ab580c595f06ecf
coomeva <34242282+coomeva@users.noreply.github.com>
2017-12-04 17:28:34 -0500

    Update index.js

--- index.js
+++ index.js
@@ -14,8 +14,10 @@ restService.use(bodyParser.json());
 
 restService.post('/echo', function(req, res) {
     var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Seems like some problem. Speak again."
-    
-    var speech2 = callConsultAssociate(speech);
+    var speech2 = 'consume service';
+     callConsultAssociate(speech).then((resultado) => {
+      speech2 = resultado;
+    });
     
    return res.json({
         speech: speech2,
